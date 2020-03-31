83272972233a508ebd9ea00f12559d8c4cbda111
Minja Kolundzija <minja.kolundzija@gmail.com>
2020-03-31 12:35:07 +0200

    Development stage task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\ChangeUsersCommand;
+use App\Console\Commands\SimulateAPICall;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        ChangeUsersCommand::class
+        ChangeUsersCommand::class,
+        SimulateAPICall::class,
     ];
 
     /**
